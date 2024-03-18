02ece49e3d6f57ae6ef8255cfd2eb4c8c7946f79
JMwangi2 <johnmnjoroge111@gmail.com>
2024-03-18 09:33:50 +0300

    commit push commit

---
+++ test1/list_assignment.py
@@ -0,0 +1,18 @@
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(1,15)
+print(my_list)
+list_2=[50,60,70]
+print(list_2)
+my_list.extend(list_2)
+print(my_list)
+my_list.pop(-1)
+print(my_list)
+my_list.sort()
+print(my_list)
+print(my_list.index(30))
+
